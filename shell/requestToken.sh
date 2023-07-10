415d31dcd6e9e19cb2cb62c3d9e875377a953c87
Francois ANDRE <francois.andre.prof@gmail.com>
2023-07-10 17:15:29 +0200

    Update requestToken.sh

--- shell/requestToken.sh
+++ shell/requestToken.sh
@@ -1,8 +1,14 @@
+#!/bin/sh
+# Require jq to be installed
+
 export TOKEN=$(curl -X POST "https://sso.aeris-data.fr/auth/realms/aeris/protocol/openid-connect/token" --insecure \
  -H "Content-Type: application/x-www-form-urlencoded" \
  -d "username=USER_EMAIL" \
  -d "password=USER_PASSWORD" \
  -d 'grant_type=password' \
  -d "client_id=aeris-public" | jq -r '.access_token')
-TOKEN="Bearer: $TOKEN"
-echo $TOKEN
+HEADER="--header=Authorization: Bearer $TOKEN"
+echo $HEADER
+
+wget "$HEADER" -qO- https://api.sedoo.fr/COMPLETE_URL_OF_THE_REQUESTED_SERVICE
+
